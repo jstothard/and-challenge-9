58f2208d1cf504e7f70cc97f945ac9561323b8f9
Jay Stothard <jtstothard@gmail.com>
2021-08-24 09:41:41 +0100

    reducedile size

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-#!/usr/bin/env node
 const list = require('./list.json')
-
 const getLongestWord = (letters) => {
     for (let i = 0, len = list.length; i < len; i++) {
-        const word = list[i]
-        const regex = new RegExp(`^[${letters}]+$`)
-        if(regex.test(word)) return word
+        if(RegExp(`^[${letters}]+$`).test(list[i])) return list[i]
     }
-    return ""
 }
-
-const letters = process.argv[2]
-
+const letters = process.argv[2]||""
 console.log(getLongestWord(letters))
